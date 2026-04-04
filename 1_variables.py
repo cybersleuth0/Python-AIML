3f65ab49c1b0c1193262e10c5d9d78de14dfe5a6
Ayush Shende <ayushshende83@gmail.com>
2026-04-04 18:27:05 +0530

    Initial commit: Add Python lecture files

---
+++ 1_variables.py
@@ -0,0 +1,7 @@
+name="Ayush"
+age=20
+height=5.8 
+
+print("My name is", name)
+print("My age is", age)
+print("My height is", height)
---
+++ 2_input.py
@@ -0,0 +1,6 @@
+# a=input("Enter value of a: ")
+# print("Value of a is: ",a)
+
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))  
+print("Sum of a and b is: ", a+b)
