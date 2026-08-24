069f06fece81c90848c7c73aaeaedd3344fd877d
ayushshende <ayushshende83@gmail.com>
2026-08-24 14:47:55 +0530

    refactor(part2): replace match/case with if/elif for traffic light demo

--- Python Fundamentals (Part 2)/2_match_case_conditional_statement.py
+++ Python Fundamentals (Part 2)/2_match_case_conditional_statement.py
@@ -1,10 +1,10 @@
-color=input("Enter color: ")
-match color:
-    case "red":
-        print("Go")
-    case "yellow":
-        print("Wait")
-    case "green":
-        print("Stop")
-    case _: # default case
-        print("Invalid color")
+color = input("Enter color: ")
+
+if color == "red":
+    print("Go")
+elif color == "yellow":
+    print("Wait")
+elif color == "green":
+    print("Stop")
+else:
+    print("Invalid color")
